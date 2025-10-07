3d6d8bcc283a60ba6f14b51aa73fa0867644c603
gaurav thakur <gthakur581@gmail.com>
2025-10-07 13:04:39 +0530

    Refactor state and city input components into separate modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useDeferredValue, useState } from "react";
 import { StateField } from "./state-field.tsx";
 import { CityField } from "./city-field.tsx";
 
 function App() {
     const [activeState, setActiveState] = useState<string | null>(null);
+    const deferredState = useDeferredValue(activeState); // not as such required in this case
 
     return (
         <main className="mx-6 mt-10 flex flex-col gap-y-6 max-w-md md:mx-auto">
@@ -11,7 +12,9 @@ function App() {
                 <li>
                     <StateField setActiveState={setActiveState} />
                 </li>
-                <li>{activeState && <CityField activeState={activeState} key={activeState} />}</li>
+                <li>
+                    {deferredState && <CityField activeState={deferredState} key={deferredState} />}
+                </li>
             </ul>
         </main>
     );
